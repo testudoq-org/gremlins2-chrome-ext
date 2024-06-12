fc72a50b503818328b4aace1cee5c823cc67cf95
testudoq <stephenhstewart@gmail.com>
2024-06-13 11:29:36 +1200

    add the map to local area

--- prebuild.js
+++ prebuild.js
@@ -3,10 +3,20 @@
 const fs = require('fs');
 const path = require('path');
 
-const source = path.resolve(__dirname, 'node_modules/gremlins.js/dist/gremlins.min.js');
-const destination = path.resolve(__dirname, 'src/content/gremlins.min.js');
+// Paths to source and destination files
+const sourceJS = path.resolve(__dirname, 'node_modules/gremlins.js/dist/gremlins.min.js');
+const destinationJS = path.resolve(__dirname, 'src/content/gremlins.min.js');
+const sourceMap = path.resolve(__dirname, 'node_modules/gremlins.js/dist/gremlins.min.js.map');
+const destinationMap = path.resolve(__dirname, 'src/content/gremlins.min.js.map');
 
-fs.copyFile(source, destination, (err) => {
+// Copy gremlins.min.js
+fs.copyFile(sourceJS, destinationJS, (err) => {
   if (err) throw err;
   console.log('gremlins.min.js was copied to src/content');
+});
+
+// Copy gremlins.min.js.map
+fs.copyFile(sourceMap, destinationMap, (err) => {
+  if (err) throw err;
+  console.log('gremlins.min.js.map was copied to src/content');
 });
